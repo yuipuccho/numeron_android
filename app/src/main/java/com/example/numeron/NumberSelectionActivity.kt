6944ba8ed227fa8a4a1c4d20e743214ac6002022
yui-yoshizawa <yui.yossy.0114@gmail.com>
2021-02-26 06:49:59 +0900

    [Add] Remove title bar

--- app/src/main/java/com/example/numeron/NumberSelectionActivity.kt
+++ app/src/main/java/com/example/numeron/NumberSelectionActivity.kt
@@ -1,9 +1,9 @@
 package com.example.numeron
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class NumberSelectionActivity : AppCompatActivity() {
+class NumberSelectionActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_selection)
