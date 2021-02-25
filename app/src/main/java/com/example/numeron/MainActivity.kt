8ecbb769666a24245bd6150b986efdd36c9044e9
yui-yoshizawa <yui.yossy.0114@gmail.com>
2021-02-26 06:39:22 +0900

    [Add] View binding

--- app/src/main/java/com/example/numeron/MainActivity.kt
+++ app/src/main/java/com/example/numeron/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.numeron
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.numeron.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // バインディング
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
