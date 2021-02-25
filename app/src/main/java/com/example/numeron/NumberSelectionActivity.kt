e547c5f18e8862c3cebe8619f6be36d24025b99b
yui-yoshizawa <yui.yossy.0114@gmail.com>
2021-02-25 09:22:20 +0900

    [Add] File of number selection activity

---
+++ app/src/main/java/com/example/numeron/NumberSelectionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.numeron
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NumberSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_selection)
+    }
+}
